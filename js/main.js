9ab47f49fa01b2ee01722f044898c2a9acfc28d0
Костя Клугман <klugmankd@gmail.com>
2017-09-27 21:14:52 +0300

    homework-3 have not done

--- js/main.js
+++ js/main.js
@@ -1,30 +1,84 @@
-var UnixOSFabric = function (isPrimary, name, caption, parent) {
-
-    var tempOS = new UnixOS(isPrimary, name, caption, parent);
-
-    tempOS.equalProto = function (prototype) {
-        return tempOS instanceof prototype;
-    };
-
-    return tempOS;
+Object.prototype.onEventDone = function (string) {
+    return eventFunction;
 };
 
-function UnixOS(isPrimary, name, caption, parent) {
-    this.isPrimary = isPrimary;
-    this.name = name;
-    this.caption = caption;
-    this.parent = parent;
-    this.equalProto = function () {};
+var block = document.getElementById('blure');
 
-    return this;
+(function () {
+    test(block, 'blur click keypress');
+})();
+
+function test(element, event) {
+    var events = {
+        "mousedown": "mousedown",
+        "mouseup": "mouseup",
+        "click": "click",
+        "focus": "focus",
+        "blur": "blur",
+        "change": "change",
+        "keydown": "keydown",
+        "keypress": "keypress",
+        "keyup" : "keyup"
+    };
+
+    if (event.indexOf(event.indexOf(events['click']) !== -1)) {
+        element.onclick = function () {
+            console.log("click");
+        }
+    }
+
+    if (event.indexOf(events['mousedown']) !== -1) {
+        element.onmousedown = function () {
+            console.log("mousedown");
+        }
+    }
+
+    if (event.indexOf(events['mouseup']) !== -1) {
+        element.onmouseup = function () {
+            console.log("mouseup");
+        }
+    }
+
+    if (event.indexOf(events["blur"]) !== -1) {
+        element.onblur = function () {
+            console.log("blur");
+        }
+    }
+
+    if (event.indexOf(events["keypress"]) !== -1) {
+        element.onkeypress = function () {
+            console.log("keypress");
+        }
+    }
+
+    if (event.indexOf(events["keyup"]) !== -1) {
+        element.onkeyup = function () {
+            console.log("keyup");
+        }
+    }
+
+    if (event.indexOf(events["keydown"]) !== -1) {
+        element.onkeydown = function () {
+            console.log("keydown");
+        }
+    }
+
+    if (event.indexOf(events["change"]) !== -1) {
+        element.onchange = function () {
+            console.log("change");
+        }
+    }
+
+    if (event.indexOf(events["focus"]) !== -1) {
+        element.onfocus = function () {
+            console.log("focus");
+        }
+    }
 }
 
-var debian = UnixOSFabric(true, 'debian', 'bla bla', null);
-
-console.log(debian);
-console.log(debian.equalProto(UnixOS));
-
-var ubuntu = UnixOSFabric(false, 'ubuntu', '');
-
-
-
+function fib(n) {
+    if (n > 1) {
+        n = fib(n - 2) + fib(n - 1);
+    }
+    return n;
+}
